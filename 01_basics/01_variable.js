25f1d0e7c36a95a0205142324812746b7f48087a
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-07-25 16:52:55 +0000

    new learning to variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 1234
+let accountEmail = "markironman@gmail.com"
+var accountPassword = "1223"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+/* prefer not to use var
+because of the issue in block scope and functional scope */
