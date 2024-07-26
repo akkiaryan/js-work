4097cb3ca53e1077025f7905884817e530d9048b
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-07-26 13:45:43 +0000

    made work progress on conversion study

---
+++ 01_basics/03_conOps.js
@@ -0,0 +1,16 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33" => 33
+// "33abc" => Nan (not a number)
+// true = 1 flase = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
