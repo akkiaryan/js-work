f76c686dcaf4be1a850df3e88e831f7ee4ed0738
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-07-26 07:51:46 +0000

    learn the basic datatypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+let name = "akki" // string
+let age = 18 //in
+let isLoggedIn = true //boolean
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// booleam => true/flase
+// null => standalone value 
+// undefined => it can be justified as it can be use if we don't want give any data
+// symbol => unique 
+
+//object
+// typeof <variable name in the constrain>
+// type of null is object but for undefined is undefined 
