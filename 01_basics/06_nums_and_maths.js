7c8b791c4f0ec92e8ca0bd937e7238375b9d0190
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-07-30 17:52:08 +0000

    understand the concept of the number and maths for js

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,12 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// ++++++++++MATHS++++++++++++++
+
+console.log(Math);
+console.log(Math.random()*10 + 1);
