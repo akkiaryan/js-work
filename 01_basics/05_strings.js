61ed0f798fc84a5fd28a09725cbd4a34f716ce85
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-07-30 15:58:07 +0000

    i have learn about the basics of the strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+//  const name = "akki"
+//  const repocount = 5
+
+//  console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+ 
+
+ const gameName =  new String('akkiaryan')
+ console.log(gameName.__proto__);
+
+
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
