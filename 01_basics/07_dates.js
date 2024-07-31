5bf43d9ba30cb10929e1c94cd4d7a91e5245890f
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-07-31 06:35:46 +0000

    date is important as it is needed more in the backend for future project

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,12 @@
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date('2023-01-24')
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
