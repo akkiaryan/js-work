9df9ed2312db717dd8a72dc54c0daac99d760d58
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-03 19:23:08 +0000

    post learing in the array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,25 @@
+const marvel_heros = ["thor","ironman","captain america"]
+
+const dc_heros = ["superman","flash","batman"]
+
+marvel_heros.push(dc_heros)
+
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// marvel_heros.concat(dc_heros);
+
+// console.log(marvel_heros);
+
+const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// we can use the spread operator as well for the concatination i.e. ...marvel_heros 
+
+console.log(Array.isArray("akki"));
+console.log(Array.from("akki"));
+console.log(Array.from({name: "akki"})); // incase of this if no input is given then the string is given empty 
+
+
+
