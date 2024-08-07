5ed6e0797d1b7fa2f6412f842acb91648ec8fad8
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-07 17:29:21 +0000

    a bit of idea on objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton -> ye apne tharha ka ek he term hai 
+
+
+// object literals
+const Jsuser ={
+    name: "AKKI",
+    age: 18,
+    location: "Jharkhand",
+    email: "akki.akki@hotmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+console.log(Jsuser.email);
+console.table(Jsuser);
