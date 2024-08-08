92b0990a9d3727eee2f5cf2073a93e4895d04475
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-08 15:01:18 +0000

    basics for the objects are done now going to start json

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,40 @@
+const bumble = {}
+
+bumble.id = "1234"
+bumble.name = "ark"
+bumble.isLoggedIn = false
+
+// console.log(bumble);
+
+// const regularUser = {
+//     email: "ark@gmail.com",
+//     fullName: {
+//         userFullname:{
+//             firstName: "akki",
+//             lastName: "aryan"
+//         }
+//     }
+// }
+// // console.log(regularUser.fullName?.userFullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3);
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+    id: 1,
+    email: "onlypan.com"
+}
+]
+users[1].email
+// console.log(bumble);
+// console.log(Object.keys(bumble));
+
+
+
