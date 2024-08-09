488ac6a688758e2e7c6950f5f93938141e864ed9
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-09 07:27:58 +0000

    added the knowlege with json and api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,40 +1,58 @@
-const bumble = {}
+// const bumble = {}
 
-bumble.id = "1234"
-bumble.name = "ark"
-bumble.isLoggedIn = false
+// bumble.id = "1234"
+// bumble.name = "ark"
+// bumble.isLoggedIn = false
 
-// console.log(bumble);
+// // console.log(bumble);
 
-// const regularUser = {
-//     email: "ark@gmail.com",
-//     fullName: {
-//         userFullname:{
-//             firstName: "akki",
-//             lastName: "aryan"
-//         }
-//     }
+// // const regularUser = {
+// //     email: "ark@gmail.com",
+// //     fullName: {
+// //         userFullname:{
+// //             firstName: "akki",
+// //             lastName: "aryan"
+// //         }
+// //     }
+// // }
+// // // console.log(regularUser.fullName?.userFullname);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "c", 4: "d"}
+
+// // const obj3 = Object.assign({}, obj1, obj2)
+// // console.log(obj3);
+
+// const obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
+
+// const users = [
+//     {
+//     id: 1,
+//     email: "onlypan.com"
 // }
-// // console.log(regularUser.fullName?.userFullname);
+// ]
+// users[1].email
+// // console.log(bumble);
+// // console.log(Object.keys(bumble));
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "c", 4: "d"}
 
-// const obj3 = Object.assign({}, obj1, obj2)
-// console.log(obj3);
-
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
-
-const users = [
-    {
-    id: 1,
-    email: "onlypan.com"
+const course = {
+    coursename : "JS KI PADAHI",
+    price : "999",
+    courseInstructor : "Hitesh"
 }
-]
-users[1].email
-// console.log(bumble);
-// console.log(Object.keys(bumble));
+const {courseInstructor: instructor} = course
 
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     name: "akki",
+//     coursename: "pata nahi",
+//     price: "free"
+// }
+
+// ending the couse with the json and api fetch
 
 
