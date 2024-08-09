ca2c06aa1765bbb1a7b7958d5c424da4a1da9ce6
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-09 18:11:22 +0000

    just get to know about business related function updates

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,17 +1,25 @@
-// function addTwoNumbers (number1, number2){
-//     let result = number1+number2
-//     return result
+// // function addTwoNumbers (number1, number2){
+// //     let result = number1+number2
+// //     return result
+// // }
+// // const result = addTwoNumbers(3,4)
+
+// // console.log("Result: ", result);
+
+// function loginUserMessage(username){
+//     if(username === undefined){
+//         console.log("Enter the username");
+//         return
+//     }
+//     return `${username} just logged in`
 // }
-// const result = addTwoNumbers(3,4)
 
-// console.log("Result: ", result);
+// console.log(loginUserMessage());
 
-function loginUserMessage(username){
-    if(username === undefined){
-        console.log("Enter the username");
-        return
-    }
-    return `${username} just logged in`
+
+
+function calculateCartPrice(...num1)//rest expansion
+{
+return num1
 }
-
-console.log(loginUserMessage());
+console.log(calculateCartPrice(200,500,400));
