0e34f62684687b23025e6fbb67794dd88877f94e
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-10 17:45:47 +0000

    get to learn about the this and arrow functions

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,37 @@
+// const user = {
+//     username : "akki",
+//     price : 999,
+
+//     welcomeMessage : function(){
+//         console.log(`${this.username}, welcome to the website`);
+        
+//     }
+// }
+// //  user.welcomeMessage()
+// //  user.username = "onlypan"
+// //  user.welcomeMessage()
+
+// console.log(this);
+
+
+
+// const chai = function(){
+//     let username = "akki"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = () => {
+//     let username = "akki"
+//     console.log(this);
+// }
+// chai()
+
+
+//implicit return
+const addTwo = (num1, num2) => (num1 + num2)
+    console.log(addTwo(3,4) ,"thalla for reason");
+
+    
+    
