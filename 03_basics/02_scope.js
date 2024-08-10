612f79e10ee03bece13b8da0b356684c0b8948b5
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-10 16:22:07 +0000

    basics of scope and hosting

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,33 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+ 
+
+function one(){
+    const username = "AKKI"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+//console.log(website);
+two()
+}
+one()
+
+
+
+if(true){
+    const username = "akki"
+    if (username === "akki"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
