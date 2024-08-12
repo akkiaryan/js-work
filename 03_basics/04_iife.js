c171f83e5ee5272812a2f35049f4c5b8f982f97c
AKKI ARYAN <142536547+akkiaryan@users.noreply.github.com>
2024-08-12 17:06:43 +0000

    basics learning to get the interview

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,9 @@
-// Immediately Invoked Fumction Expression (IIFE)
+// Immediately Invoked Function Expression (IIFE)
 
 (function chai(){
     console.log(`DB CONNECTED`);
 })()//iife constraints
+
+
+// if we needed to add one more of the iife then add the ((data_name) => {
+//    })()
